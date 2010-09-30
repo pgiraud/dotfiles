ea8f30cefcf90ef3f8dd237709c2d11f9c0dc6eb
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2010-09-30 23:02:28 +0200

    Command to set terminal colors

--- install.py
+++ install.py
@@ -10,3 +10,6 @@ for f in os.listdir('.'):
             os.unlink(path)
         os.symlink(os.path.abspath(f), path)
         print 'create link for %s' % (path)
+
+# to get better colors in the terminal, launch the following command
+# gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/palette "#000000000000:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2:#4CCC4CCC4CCC:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2" 
