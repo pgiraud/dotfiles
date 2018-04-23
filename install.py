267f86992c33074b96cf0be16cfd31af8d0ea7e4
Pierre GIRAUD <pierre.giraud@dalibo.com>
2018-04-23 16:01:36 +0200

    NeoVIM

--- install.py
+++ install.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python
-import os, fnmatch, shutil
+#! /usr/bin/env python
 
-exclude = [ '*.sw*', '.git', 'install.*', '.gitmodules' ]
+import os
+import fnmatch
+
+exclude = ['*.sw*', '.git', 'install.*', '.gitmodules']
 
 for f in os.listdir('.'):
     if not any(fnmatch.fnmatch(f, p) for p in exclude):
@@ -11,5 +13,11 @@ for f in os.listdir('.'):
         os.symlink(os.path.abspath(f), path)
         print 'create link for %s' % (path)
 
+f = 'nvim.init'
+path = os.path.expanduser('~/.config/nvim/init.vim')
+if os.path.isfile(path) or os.path.isdir(path):
+    os.unlink(path)
+os.symlink(os.path.abspath('nvim.init'), path)
+
 # to get better colors in the terminal, launch the following command
-# gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/palette "#000000000000:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2:#4CCC4CCC4CCC:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2" 
+# gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/palette "#000000000000:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2:#4CCC4CCC4CCC:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2"
